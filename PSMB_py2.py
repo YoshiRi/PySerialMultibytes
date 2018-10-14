d34d84eeb2d96c65a78f7fbc457cb1628034a602
YoshiRi <yoshiyoshidetteiu@gmail.com>
2018-10-14 10:00:35 -0700

    add function to send time

---
+++ PSMB_py2.py
@@ -0,0 +1,30 @@
+import serial
+import struct
+import time
+
+class Ser_mbyte():
+    def __init__(self,PORT='/dev/ttyUSB0'):
+        self.ser = serial.Serial(PORT,115200,bytesize=8,stopbits=1,timeout=2,write_timeout =2)
+        
+    def sendfloat(self,data):
+        print(data)
+        sdata = struct.pack('>f', float(data))
+        self.ser.write(sdata)
+    
+    def __del__(self):
+        self.ser.close()
+
+
+if __name__ == '__main__':
+    ser = Ser_mbyte()
+    start = time.time()
+    
+    while True:
+        try:
+            now = time.time() - start
+            ser.sendfloat(now)
+            time.sleep(0.001)# 1ms sleep
+            print(now)
+            
+        except KeyboardInterrupt:
+            break
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PySerialMultibytes
 Python send multibyte data to DSP ( mainly considering C,C++ )
+
+## Version isssue
+This code will be finally implemented to ROS node.
+So, python2.7 is considered as main environment.
+
+# TODO
+
+- Multibyte sending with list
+- Multibyte receiving and reconstruction
+
